e2290802e29dde4d1ff66e464b0a6dcac52e93a1
unknown <114523969+pedroditersimon@users.noreply.github.com>
2024-09-18 00:00:05 -0300

    ContactServiceTest

---
+++ SchoolNotes.Test/Services/ContactServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolNotes.Test.Services;
+
+internal class ContactServiceTest
+{
+    [SetUp]
+    public void SetUp() { }
+
+    [Test]
+    public async Task AssignTeacher()
+    { }
+}
