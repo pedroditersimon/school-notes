25506a6b8063422bcbdb2e20b54b1c6f126e52d9
unknown <114523969+pedroditersimon@users.noreply.github.com>
2024-09-17 23:57:42 -0300

    added two test

--- SchoolNotes.Test/Services/CourseSessionServiceTest.cs
+++ SchoolNotes.Test/Services/CourseSessionServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using SchoolNotes.API.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolNotes.API.Models;
 using SchoolNotes.API.Services;
 
 namespace SchoolNotes.Test.Services;
@@ -31,5 +32,69 @@ internal class CourseSessionServiceTest
 
         Assert.IsNotNull(courseSession);
         Assert.AreEqual(TestsHelper.GUID01, courseSession.TeacherID);
+    }
+
+
+    [Test]
+    public async Task GetByStudentID()
+    {
+        IUnitOfWork unitOfWork = TestsHelper.CreateInMemoryUnitOfWork();
+        CourseSessionService service = new(unitOfWork);
+
+        await unitOfWork.StudentRepository.Create(new Student()
+        {
+            ID = TestsHelper.GUID01
+        });
+
+        await unitOfWork.CourseRepository.Create(new Course()
+        {
+            ID = TestsHelper.GUID02,
+        });
+
+        await unitOfWork.CourseSessionRepository.Create(new CourseSession()
+        {
+            ID = TestsHelper.GUID03,
+            CourseID = TestsHelper.GUID02
+        });
+
+        await unitOfWork.CourseSessionStudentRepository.Create(new CourseSessionStudent()
+        {
+            ID = TestsHelper.GUID04,
+            CourseSessionID = TestsHelper.GUID03,
+            StudentID = TestsHelper.GUID01
+        });
+        await unitOfWork.Save();
+
+        List<CourseSession> courseSessions = await service.GetByStudentID(TestsHelper.GUID01).ToListAsync();
+
+        Assert.IsNotNull(courseSessions);
+        Assert.Greater(courseSessions.Count, 0);
+        Assert.AreEqual(1, courseSessions.Count);
+    }
+
+    [Test]
+    public async Task GetByCourseID()
+    {
+        IUnitOfWork unitOfWork = TestsHelper.CreateInMemoryUnitOfWork();
+        CourseSessionService service = new(unitOfWork);
+
+        await unitOfWork.CourseRepository.Create(new Course()
+        {
+            ID = TestsHelper.GUID01,
+        });
+
+        await unitOfWork.CourseSessionRepository.Create(new CourseSession()
+        {
+            ID = TestsHelper.GUID02,
+            CourseID = TestsHelper.GUID01
+        });
+
+        await unitOfWork.Save();
+
+        List<CourseSession> courseSessions = await service.GetByCourseID(TestsHelper.GUID01).ToListAsync();
+
+        Assert.IsNotNull(courseSessions);
+        Assert.Greater(courseSessions.Count, 0);
+        Assert.AreEqual(1, courseSessions.Count);
     }
 }
