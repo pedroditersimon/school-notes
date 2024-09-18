bcf6cb53c3dbefbd021f2db6a6fbf987bc6d485a
unknown <114523969+pedroditersimon@users.noreply.github.com>
2024-09-18 02:00:59 -0300

    created contact DTO and Mapper

---
+++ SchoolNotes.API/Mappers/ContactMapper.cs
@@ -0,0 +1,17 @@
+﻿using SchoolNotes.API.Models;
+using SchoolNotes.API.Models.DTOs;
+
+namespace SchoolNotes.API.Mappers;
+
+public static class ContactMapper
+{
+    public static ContactDTO ToDTO(this Contact contact)
+    {
+        return new()
+        {
+            FirstName = contact.FirstName,
+            LastName = contact.LastName,
+            DNI = contact.DNI
+        };
+    }
+}
---
+++ SchoolNotes.API/Models/DTOs/ContactDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolNotes.API.Models.DTOs;
+
+public class ContactDTO
+{
+    public string? FirstName { get; set; }   // Juan
+    public string? LastName { get; set; }    // Perez
+
+    public string? DNI { get; set; }
+}
