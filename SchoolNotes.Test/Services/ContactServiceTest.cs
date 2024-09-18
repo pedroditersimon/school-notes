9783e6242dab005e95b0ae2c28249d1c7fb8d033
unknown <114523969+pedroditersimon@users.noreply.github.com>
2024-09-18 00:22:27 -0300

    added contact service test

--- SchoolNotes.Test/Services/ContactServiceTest.cs
+++ SchoolNotes.Test/Services/ContactServiceTest.cs
@@ -1,11 +1,77 @@
-﻿namespace SchoolNotes.Test.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolNotes.API.Models;
+using SchoolNotes.API.Services;
+
+namespace SchoolNotes.Test.Services;
 
 internal class ContactServiceTest
 {
     [SetUp]
-    public void SetUp() { }
+    public void SetUp()
+    {
+    }
 
     [Test]
-    public async Task AssignTeacher()
-    { }
+    public async Task SearchByDNI()
+    {
+        IUnitOfWork unitOfWork = TestsHelper.CreateInMemoryUnitOfWork();
+        ContactService service = new(unitOfWork);
+
+        await service.Create(new Contact()
+        {
+            ID = TestsHelper.GUID01,
+            DNI = "123456789"
+        });
+        await service.Create(new Contact()
+        {
+            ID = TestsHelper.GUID02,
+            DNI = "987654321"
+        });
+        await service.Create(new Contact()
+        {
+            ID = TestsHelper.GUID03,
+            DNI = "123000321"
+        });
+        await unitOfWork.Save();
+
+        List<Contact> contacts = await service.SearchByDNI("123456789").ToListAsync();
+        Contact? resultContact = contacts.FirstOrDefault();
+
+        Assert.AreEqual(1, contacts.Count);
+
+        Assert.IsNotNull(resultContact);
+        Assert.AreEqual(TestsHelper.GUID01, resultContact.ID);
+        Assert.AreEqual("123456789", resultContact.DNI);
+    }
+
+
+    [Test]
+    public async Task GetByDNI()
+    {
+        IUnitOfWork unitOfWork = TestsHelper.CreateInMemoryUnitOfWork();
+        ContactService service = new(unitOfWork);
+
+        await service.Create(new Contact()
+        {
+            ID = TestsHelper.GUID01,
+            DNI = "123456789"
+        });
+        await service.Create(new Contact()
+        {
+            ID = TestsHelper.GUID02,
+            DNI = "987654321"
+        });
+        await service.Create(new Contact()
+        {
+            ID = TestsHelper.GUID03,
+            DNI = "123000321"
+        });
+        await unitOfWork.Save();
+
+        Contact? contact = await service.GetByDNI("123456789");
+
+        Assert.IsNotNull(contact);
+        Assert.AreEqual(TestsHelper.GUID01, contact.ID);
+        Assert.AreEqual("123456789", contact.DNI);
+    }
 }
